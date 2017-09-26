fa087f269261f224eafde5b4272afa919a8ea966
Gene Huh <ehuh@teamexos.com>
2017-09-25 22:58:40 -0700

    Added edit view to View

--- src/elm/View.elm
+++ src/elm/View.elm
@@ -5,6 +5,7 @@ import Models exposing (Model, CollectibleId)
 import Models exposing (Model)
 import Msgs exposing (Msg)
 import Collectibles.List
+import Collectibles.Edit
 import RemoteData
 
 
@@ -14,6 +15,9 @@ page model =
         Models.CollectiblesRoute ->
             Collectibles.List.view model.collectibles
 
+        Models.CollectibleRoute id ->
+            collectibleEditPage model id
+
         Models.NotFoundRoute ->
             notFoundView
 
@@ -22,6 +26,33 @@ view : Model -> Html Msg
 view model =
     div []
         [ page model ]
+
+
+collectibleEditPage : Model -> CollectibleId -> Html Msg
+collectibleEditPage model collectibleId =
+    case model.collectibles of
+        RemoteData.NotAsked ->
+            text ""
+
+        RemoteData.Loading ->
+            text "Loading ..."
+
+        RemoteData.Success collectibles ->
+            let
+                maybeCollectible =
+                    collectibles
+                        |> List.filter (\collectible -> collectible.id == collectibleId)
+                        |> List.head
+            in
+                case maybeCollectible of
+                    Just collectible ->
+                        Collectibles.Edit.view collectible
+
+                    Nothing ->
+                        notFoundView
+
+        RemoteData.Failure err ->
+            text (toString err)
 
 
 notFoundView : Html msg
