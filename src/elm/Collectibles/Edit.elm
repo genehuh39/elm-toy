04e7c59796440f184d1a1f19f3a78db394dd591c
Gene Huh <ehuh@teamexos.com>
2017-10-01 23:51:07 -0700

    started adding fields to form

--- src/elm/Collectibles/Edit.elm
+++ src/elm/Collectibles/Edit.elm
@@ -1,18 +1,25 @@
 module Collectibles.Edit exposing (..)
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
 import Models exposing (Collectible)
 import Msgs exposing (Msg)
 import Routing exposing (collectiblesPath)
+import Html.Events exposing (onInput)
 
 
 view : Collectible -> Html.Html Msg
 view model =
     div []
-        [ text "Form" ]
+        [ text "Form"
+        , form model
+        ]
 
 
 form : Collectible -> Html.Html Msg
 form collectible =
     div []
-        [ h1 [] [ text collectible.manufacturer ] ]
+        [ div [] [ text "Manufacturer ", input [ type_ "text", placeholder "manufacturer", value collectible.manufacturer ] [] ]
+        , div [] [ text "Series ", input [ type_ "text", placeholder "series", value collectible.series ] [] ]
+        , div [] [ text "Name", input [ type_ "text", placeholder "name", value collectible.name ] [] ]
+        ]
