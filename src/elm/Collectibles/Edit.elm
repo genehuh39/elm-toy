5f3234ba5cf6a2fe3f2b64982e0bad8f255cc181
Gene Huh <ehuh@teamexos.com>
2017-10-04 22:38:12 -0700

    add form styling to edit

--- src/elm/Collectibles/Edit.elm
+++ src/elm/Collectibles/Edit.elm
@@ -18,9 +18,9 @@ view model =
 
 form : Collectible -> Html.Html Msg
 form collectible =
-    div []
-        [ div [] [ label [] [ text " Manufacturer " ], input [ type_ "text", placeholder "manufacturer", value collectible.manufacturer ] [] ]
-        , div [] [ label [] [ text " Series " ], input [ type_ "text", placeholder "series", value collectible.series ] [] ]
-        , div [] [ label [] [ text " Name " ], input [ type_ "text", placeholder "name", value collectible.name ] [] ]
-        , div [] [ label [] [ text "List Price $" ], input [ type_ "text", placeholder "list price", value (toString collectible.listPrice) ] [] ]
+    div [ class "pa4 black-80" ]
+        [ div [ class "mt3" ] [ label [ class "db fw6 lh-copy f6" ] [ text " Manufacturer " ], input [ type_ "text", placeholder "manufacturer", value collectible.manufacturer, class "pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" ] [] ]
+        , div [ class "mt3" ] [ label [ class "db fw6 lh-copy f6" ] [ text " Series " ], input [ type_ "text", placeholder "series", value collectible.series, class "pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" ] [] ]
+        , div [ class "mt3" ] [ label [ class "db fw6 lh-copy f6" ] [ text " Name " ], input [ type_ "text", placeholder "name", value collectible.name, class "pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" ] [] ]
+        , div [ class "mt3" ] [ label [ class "db fw6 lh-copy f6" ] [ text "List Price $" ], input [ type_ "text", placeholder "list price", value (toString collectible.listPrice), class "pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" ] [] ]
         ]
