24adb66c48bd131c17a22acc7adae6d182868a92
Gene Huh <ehuh@teamexos.com>
2017-10-10 20:31:01 -0700

    added descripion field to the form

--- src/elm/Collectibles/Edit.elm
+++ src/elm/Collectibles/Edit.elm
@@ -22,5 +22,6 @@ form collectible =
         [ div [ class "mt3" ] [ label [ class "db fw6 lh-copy f6" ] [ text " Manufacturer " ], input [ type_ "text", placeholder "manufacturer", value collectible.manufacturer, class "pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" ] [] ]
         , div [ class "mt3" ] [ label [ class "db fw6 lh-copy f6" ] [ text " Series " ], input [ type_ "text", placeholder "series", value collectible.series, class "pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" ] [] ]
         , div [ class "mt3" ] [ label [ class "db fw6 lh-copy f6" ] [ text " Name " ], input [ type_ "text", placeholder "name", value collectible.name, class "pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" ] [] ]
-        , div [ class "mt3" ] [ label [ class "db fw6 lh-copy f6" ] [ text "List Price $" ], input [ type_ "text", placeholder "list price", value (toString collectible.listPrice), class "pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" ] [] ]
+        , div [ class "mt3" ] [ label [ class "db fw6 lh-copy f6" ] [ text " List Price $" ], input [ type_ "text", placeholder "list price", value (toString collectible.listPrice), class "pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" ] [] ]
+        , div [ class "mt3" ] [ label [ class "db fw6 lh-copy f6" ] [ text " Description " ], input [ type_ "text", placeholder "description", value (toString collectible.description), class "pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" ] [] ]
         ]
