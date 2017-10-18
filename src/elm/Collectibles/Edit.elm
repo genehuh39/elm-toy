a84655623a0342d5bf7442794efc645805cce9ae
Gene Huh <ehuh@teamexos.com>
2017-10-17 21:22:33 -0700

    Add saction button and form width change

--- src/elm/Collectibles/Edit.elm
+++ src/elm/Collectibles/Edit.elm
@@ -10,9 +10,8 @@ import Html.Events exposing (onInput)
 
 view : Collectible -> Html.Html Msg
 view model =
-    div []
-        [ text "Form"
-        , form model
+    div [ class "w-70" ]
+        [ form model
         ]
 
 
@@ -24,4 +23,12 @@ form collectible =
         , div [ class "mt3" ] [ label [ class "db fw6 lh-copy f6" ] [ text " Name " ], input [ type_ "text", placeholder "name", value collectible.name, class "pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" ] [] ]
         , div [ class "mt3" ] [ label [ class "db fw6 lh-copy f6" ] [ text " List Price $" ], input [ type_ "text", placeholder "list price", value (toString collectible.listPrice), class "pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" ] [] ]
         , div [ class "mt3" ] [ label [ class "db fw6 lh-copy f6" ] [ text " Description " ], input [ type_ "text", placeholder "description", value (toString collectible.description), class "pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" ] [] ]
+        , div [] [ actionBtn collectible ]
+        ]
+
+
+actionBtn : Collectible -> Html.Html Msg
+actionBtn collectible =
+    div [ class "pa3" ]
+        [ button [ class "f6 link dim br1 ba bw1 ph3 pv2 mb2 dib black" ] [ text "save" ]
         ]
